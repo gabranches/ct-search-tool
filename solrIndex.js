965a3d41bcb1e55c1b93ed30e26d125567dcc931
Gustavo Abranches <gabranches@gmail.com>
2016-09-30 11:13:43 -0400

    minor changes and comments

--- solrIndex.js
+++ solrIndex.js
@@ -3,8 +3,8 @@ var data = require('./data/output.json');
 var studies = data.studies.clinical_study;
 var solrAPI = require('./lib/solr-api');
 
-// Create mappings for nested objects
-var mappings = {
+// Create map for nested objects
+var map = {
     'id_info.0.nct_id.0'                        : 'nct_id',
     'required_header.0.url.0'                   : 'url',
     'official_title.0'                          : 'official_title',
@@ -27,10 +27,9 @@ var mappings = {
 
 var studiesFlattened = [];
 
-
 // Flatten each study
-for (i in studies) {
-    studiesFlattened.push(jsonUtils.flatten(studies[i], mappings));
+for (study in studies) {
+    studiesFlattened.push(jsonUtils.flatten(studies[study], map));
 }
 
 // Index changes in solr
