278297ddcb16e20feedf10e7ffa0c79a7c6be32e
Gustavo Abranches <gabranches@gmail.com>
2016-10-14 08:18:20 -0400

    changed mapping to array of objects

--- solrIndex.js
+++ solrIndex.js
@@ -3,35 +3,104 @@ var data = require('./data/output.json');
 var studies = data.studies.clinical_study;
 var solrAPI = require('./lib/solr-api');
 
-// Create map for nested objects
-var map = {
-    'id_info.0.nct_id.0'                        : 'nct_id',
-    'required_header.0.url.0'                   : 'url',
-    'official_title.0'                          : 'official_title',
-    'phase.0'                                   : 'phase',
-    'brief_summary.0.textblock.0'               : 'brief_summary',
-    'eligibility.0.healthy_volunteers.0'        : 'healthy_volunteers',
-    'eligibility.0.gender.0'                    : 'gender',
-    'eligibility.0.criteria.0.textblock.0'      : 'inclusion_criteria',
-    'eligibility.0.minimum_age.0'               : 'minimum_age',
-    'eligibility.0.maximum_age.0'               : 'maximum_age',
-    'keyword'                                   : 'keywords',
-    'lastchanged_date.0'                        : 'last_updated',
-    'overall_contact.0'                         : 'contact',
-    'detailed_description.0.textblock.0'        : 'detailed_description',
-    'brief_title.0'                             : 'brief_title',
-    'overall_official'                          : 'overall_officials',
-    'primary_outcome'                           : 'primary_outcome',
-    'secondary_outcome'                         : 'secondary_outcome'
-}
+var map = [
+    {
+        tag: 'id_info.0.nct_id.0',
+        mapping: 'nct_id',
+        dynamic: false
+    },
+    {
+        tag: 'required_header.0.url.0' ,
+        mapping: 'url',
+        dynamic: false
+    },
+    {
+        tag: 'official_title.0',
+        mapping: 'official_title',
+        dynamic: false
+    },
+    {
+        tag: 'phase.0',
+        mapping: 'phase',
+        dynamic: false
+    },
+    {
+        tag: 'brief_summary.0.textblock.0',
+        mapping: 'brief_summary',
+        dynamic: false
+    },
+    {
+        tag: 'eligibility.0.healthy_volunteers.0' ,
+        mapping: 'healthy_volunteers',
+        dynamic: false
+    },
+    {
+        tag: 'eligibility.0.gender.0',
+        mapping: 'gender',
+        dynamic: false
+    },
+    {
+        tag: 'eligibility.0.criteria.0.textblock.0',
+        mapping: 'inclusion_criteria',
+        dynamic: false
+    },
+    {
+        tag: 'eligibility.0.minimum_age.0',
+        mapping: 'minimum_age',
+        dynamic: false
+    },
+    {
+        tag: 'eligibility.0.maximum_age.0',
+        mapping: 'maximum_age',
+        dynamic: false
+    },
+    {
+        tag: 'keyword',
+        mapping: 'keywords',
+        dynamic: false
+    },
+    {
+        tag: 'lastchanged_date.0',
+        mapping: 'last_updated',
+        dynamic: false
+    },
+    {
+        tag: 'overall_contact.0',
+        mapping: 'contact',
+        dynamic: false
+    },
+    {
+        tag: 'detailed_description.0.textblock.0',
+        mapping: 'detailed_description',
+        dynamic: false
+    },
+    {
+        tag: 'brief_title.0',
+        mapping: 'brief_title',
+        dynamic: false
+    },
+    {
+        tag: 'overall_official',
+        mapping: 'overall_officials',
+        dynamic: true
+    },
+    {
+        tag: 'primary_outcome',
+        mapping: 'primary_outcome',
+        dynamic: true
+    },
+    {
+        tag: 'secondary_outcome',
+        mapping: 'secondary_outcome',
+        dynamic: true
+    }
+];
 
-
-var dynamicFields = ['overall_officials', 'primary_outcome', 'secondary_outcome'];
 var studiesFlattened = [];
 
 // Flatten each study
 for (study in studies) {
-    studiesFlattened.push(jsonUtils.flatten(studies[study], map, dynamicFields));
+    studiesFlattened.push(jsonUtils.flatten(studies[study], map));
 }
 
 // Index changes in solr
